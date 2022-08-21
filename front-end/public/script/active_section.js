69ea207517893826c3b6158372909412cd326c14
mattp04 <matiaspavon8@gmail.com>
2022-08-21 04:40:13 -0300

    use IntersectionObserver to detect active section

--- front-end/public/script/active_section.js
+++ front-end/public/script/active_section.js
@@ -8,52 +8,28 @@ function parseNodeList(nodeList) {
 }
 
 let main_container = document.querySelector("main");
-let scrollable_container = document.scrollingElement;
 let navBar = document.querySelector("nav");
-console.log(scrollable_container);
 let sections = parseNodeList(main_container.querySelectorAll("section"));
 let navIcons = parseNodeList(navBar.querySelectorAll(".navigation-icon"));
 
-function setActiveNavIcon(index) {
+function setActiveNavIcon(icon_name) {
     // Remove .active class to all icons
     navIcons.forEach((icon) => {
-        icon.classList.remove("active");
-    });
-
-    // Set class .active
-    navIcons[index].classList.add("active");
-}
-
-const scroll_indicator = document.querySelector(".scroll-position-indicator");
-function doSomething(scroll_pos) {
-    let max_scroll_pos = scrollable_container.scrollHeight - scrollable_container.clientHeight;
-    let scroll_progress = (scroll_pos / max_scroll_pos);
-    
-    let main_scroll_top = (scrollable_container.scrollHeight * scroll_progress).toFixed(0);
-    // scroll_indicator.style.top = ((main_container.scrollHeight-26) * scroll_progress) + "px";
-    //scroll_indicator.textContent = `Position: ${main_scroll_top} / ${main_container.scrollHeight}`;
-    // scroll_indicator.textContent = `Position: ${scroll_pos} / ${max_scroll_position} (${scroll_progress.toFixed(2)})`;
-    for (let i=0; i<sections.length; i++) {
-        let section = sections[i];
-        if((section.offsetTop+section.offsetHeight) > main_scroll_top) {
-            setActiveNavIcon(i);
-            break;
+        if (icon.id === icon_name) {
+            icon.classList.add("active");
+        } else {
+            icon.classList.remove("active");
         }
-    }
+    });
 }
 
-let last_known_scroll_position = 0;
-let ticking = false;
+const sectionObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            setActiveNavIcon(entry.target.id + "_icon");
+        }
+    })
+}, { threshold: 0.2 });
 
 
-doSomething(scrollable_container.scrollTop);
-window.addEventListener('scroll', function (e) {
-    last_known_scroll_position = scrollable_container.scrollTop;
-    if (!ticking) {
-        window.requestAnimationFrame(function () {
-            doSomething(last_known_scroll_position);
-            ticking = false;
-        });
-    }
-    ticking = true;
-});
+sections.forEach(section => sectionObserver.observe(section));
